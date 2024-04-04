3c9e08f68ad46b52338a5a050efa5d5667bb73ac
nalpu <160195073+nalpu@users.noreply.github.com>
2024-04-04 19:10:08 +0000

    Delete time.lua

--- diewald_CV_kit/src/diewald_CV_kit/blobdetection/Contour.java
+++ diewald_CV_kit/src/diewald_CV_kit/blobdetection/Contour.java
@@ -9,4 +9,16 @@
  *   (C) 2012    Thomas Diewald
  *               http://www.thomasdiewald.com
  *   
+ *   last built: 12/13/2012
+ *   
+ *   download:   http://thomasdiewald.com/processing/libraries/diewald_CV_kit/
+ *   source:     https://github.com/diwi/diewald_CV_kit 
+ *   
+ *   tested OS:  osx,windows
+ *   processing: 1.5.1, 2.07
  *
+ *
+ *
+ *
+ * This source is free software; you can redistribute it and/or modify
+ * it under the 
