cfcd0308beb89ef3a8283df81b1f9a58e7b6be8d
nalpu <160195073+nalpu@users.noreply.github.com>
2024-04-27 12:15:03 +0000

    gallery update

---
+++ diewald_CV_kit/src/diewald_CV_kit/libraryinfo/LibraryInfo.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ * diewald_CV_kit v1.1
+ * 
+ * this library provides simple tools needed in computer-vision.
+ * 
+ * 
+ * 
+ *   (C) 2012    Thomas Diewald
+ *               http://www.thomasdiewald.com
+ *   
+ *   last built: 12/13/2012
+ *   
+ *   download:   http://thomasdiew
