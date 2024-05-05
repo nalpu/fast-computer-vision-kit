f0073ab967d15636ce797b193dd12630bf60b68f
nalpu <160195073+nalpu@users.noreply.github.com>
2024-05-05 10:57:29 +0000

    Changed screenshot

--- diewald_CV_kit/src/diewald_CV_kit/utility/Polyline.java
+++ diewald_CV_kit/src/diewald_CV_kit/utility/Polyline.java
@@ -133,3 +133,314 @@ public abstract class Polyline {
     area *= (area < 0) ? -.5f : +.5f;
     return area;
   }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  //----------------------------------------------------------------------------
+  // LENGTH
+  //----------------------------------------------------------------------------
+  /**
+   * returns the length of the polyline
+   * 
+   * @param polyline the polyline to get the length from
+   * @return returns the length of the polyline
+   */
+  public static final float LENGTH( Pixel[] polyline ){
+
+    Pixel p1, p2;
+    float length = 0;
+    for(int i = 0; i < polyline.length-1; i++){
+      p1 = polyline[i];
+      p2 = polyline[i+1];
+      length += distance(p1, p2);
+    }
+    return length;
+  }
+  
+  
+  /**
+   * returns the length of the polyline
+   * 
+   * @param polyline the polyline to get the length from
+   * @return returns the length of the polyline
+   */
+  public static final float LENGTH( List<Pixel> polyline ){
+    List<Pixel> list = polyline;
+    Pixel p1, p2;
+    float length = 0;
+    for(int i = 0; i < list.size()-1; i++){
+      p1 = list.get(i);
+      p2 = list.get(i+1);
+      length += distance(p1, p2);
+    }
+    return length;
+  }
+  
+  /**
+   * returns the length of the contour
+   * 
+   * @param contour the polyline to get the length from
+   * @return returns the length of the contour
+   */
+  public static final float LENGTH( Contour contour ){
+    return LENGTH(contour.getPixels());
+  }
+  
+  /**
+   * returns the length of the convex_hull
+   * 
+   * @param convex_hull the polyline to get the length from
+   * @return returns the length of the convex_hull
+   */
+  public static final float LENGTH( ConvexHullDiwi convex_hull ){
+    DoubleLinkedList<Pixel> list = convex_hull.get();
+    list.gotoFirst();
+    Pixel p1, p2;
+    float length = 0;
+    for(int idx = 0; idx < list.size(); idx++){
+      p1 = list.getCurrentNode().get();
+      list.gotoNext();
+      p2 = list.getCurrentNode().get();
+      length += distance(p1, p2);
+    }
+    return length;
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  //----------------------------------------------------------------------------
+  // SIMPLIFY
+  //----------------------------------------------------------------------------
+  /**
+   *  
+   * returns a new simplified polyline, from a given contour
+   *
+   * @param contour to simplify
+   * @param step    the number of vertices in a row, that are checked for the maximum offset
+   * @param max_offset maximum offset a vertice can have 
+   * @return new simplified polygon
+   */
+  public static final ArrayList<Pixel> SIMPLIFY( Contour contour, int step,  float max_offset ){
+    return SIMPLIFY(contour.getPixels(), step, max_offset);
+  }
+
+  /**
+   *  
+   * returns a new simplified polyline, from a given convex_hull
+   *
+   * @param convex_hull to simplify
+   * @param step    the number of vertices in a row, that are checked for the maximum offset
+   * @param max_offset maximum offset a vertice can have 
+   * @return new simplified polygon
+   */
+  public static final ArrayList<Pixel> SIMPLIFY( ConvexHullDiwi convex_hull, int step,  float max_offset ){
+    return SIMPLIFY(convex_hull.get().toList(), step, max_offset);
+  }
+  /**
+   *  
+   * returns a new simplified polyline, from a given polyline
+   *
+   * @param polyline to simplify
+   * @param step    the number of vertices in a row, that are checked for the maximum offset
+   * @param max_offset maximum offset a vertice can have 
+   * @return new simplified polygon
+   */
+  public static final ArrayList<Pixel> SIMPLIFY( List<Pixel> polyline, int step,  float max_offset ){
+    ArrayList<Pixel> poly_simp = new ArrayList<Pixel>();
+    int index_cur  = 0;
+    int index_next = 0;
+    poly_simp.add(polyline.get(index_cur));
+    
+    while( index_cur != polyline.size()-1 ){
+      index_next = ((index_cur + step) >= polyline.size()) ? (polyline.size()-1) :  (index_cur + step) ;
+
+      Pixel p_cur  = polyline.get( index_cur  );
+      Pixel p_next = polyline.get( index_next );
+
+      while( (++index_cur < index_next) &&  (max_offset > abs(distancePoint2Line(p_cur, p_next, polyline.get(index_cur))) ) );
+      poly_simp.add(polyline.get(index_cur));
+    }
+    return poly_simp;
+  }
+  
+  /**
+   *  
+   * returns a new simplified polyline, from a given polyline
+   *
+   * @param polyline to simplify
+   * @param step    the number of vertices in a row, that are checked for the maximum offset
+   * @param max_offset maximum offset a vertice can have 
+   * @return new simplified polygon
+   */
+  public static final ArrayList<Pixel> SIMPLIFY( Pixel[] polyline, int step,  float max_offset ){
+    ArrayList<Pixel> poly_simp = new ArrayList<Pixel>();
+    int index_cur  = 0;
+    int index_next = 0;
+    poly_simp.add(polyline[index_cur]);
+    
+    while( index_cur != polyline.length-1 ){
+      index_next = ((index_cur + step) >= polyline.length) ? (polyline.length-1) :  (index_cur + step) ;
+
+      Pixel p_cur  = polyline[index_cur];
+      Pixel p_next = polyline[index_next];
+
+      while( (++index_cur < index_next) &&  (max_offset > abs(distancePoint2Line(p_cur, p_next, polyline[index_cur])) ) );
+      poly_simp.add(polyline[index_cur]);
+    }
+    return poly_simp;
+  }
+  
+
+  
+  
+  //----------------------------------------------------------------------------
+  // INSIDE
+  //----------------------------------------------------------------------------
+
+  /**
+   * 
+   * check if a given coordinate pair is inside a polyline.
+   * this method uses java.awt.Polygon;
+   */
+  public static final boolean INSIDE( Pixel[] polyline, int x, int y){
+    int[] x_coords = new int[polyline.length];
+    int[] y_coords = new int[polyline.length];
+    int idx = 0;
+    for( Pixel p : polyline ){
+      x_coords[idx] = p.x_;
+      y_coords[idx] = p.y_;
+      idx++;
+    }
+    Polygon polygon = new Polygon(x_coords, y_coords, polyline.length);
+    return polygon.contains(x, y);
+  }
+  
+  /**
+   * 
+   * check if a given coordinate pair is inside a polyline.
+   * this method uses java.awt.Polygon;
+   */
+  public static final boolean INSIDE( List<Pixel> polyline, int x, int y){
+    int[] x_coords = new int[polyline.size()];
+    int[] y_coords = new int[polyline.size()];
+    int idx = 0;
+    for( Pixel p : polyline ){
+      x_coords[idx] = p.x_;
+      y_coords[idx] = p.y_;
+      idx++;
+    }
+    Polygon polygon = new Polygon(x_coords, y_coords, polyline.size());
+    return polygon.contains(x, y);
+  }
+  
+  /**
+   * 
+   * check if a given coordinate pair is inside a contour.
+   * this method uses java.awt.Polygon;
+   */
+  public static final boolean INSIDE( Contour contour, int x, int y){
+    return INSIDE(contour.getPixels(), x, y);
+  }
+  /**
+   * 
+   * check if a given coordinate pair is inside a convex_hull.
+   * this method uses java.awt.Polygon;
+   */
+  public static final boolean INSIDE( ConvexHullDiwi convex_hull, int x, int y){
+    DoubleLinkedList<Pixel> list = convex_hull.get();
+    list.gotoFirst();
+    Pixel p;
+    int[] x_coords = new int[list.size()];
+    int[] y_coords = new int[list.size()];
+    
+
+    for(int idx = 0; idx < list.size(); idx++, list.gotoNext()){
+      p = list.getCurrentNode().get();
+      x_coords[idx] = p.x_;
+      y_coords[idx] = p.y_;
+    }
+    Polygon polygon = new Polygon(x_coords, y_coords, list.size());
+    return polygon.contains(x, y);
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  /**
+   * 
+   * returns the distance between two pixels
+   * 
+   */
+  public static final double distance(Pixel p1, Pixel p2){
+    return distance(p1.x_, p1.y_, p2.x_, p2.y_ );
+  }
+  
+  /**
+   * 
+   * returns the distance between two coordinate pairs
+   * 
+   */
+  public static final double distance(float x1, float y1,float x2, float y2){
+    float dx = x1-x2;
+    float dy = y1-y2;
+    return Math.sqrt(dx*dx+ dy*dy);
+  }
+
+    
+  /**
+   * 
+   * returns the shortest distance of a point(p3) to a line (p1-p2)
+   * 
+   */
+  public static final float distancePoint2Line(Pixel p1, Pixel p2, Pixel p3 ){
+    float x1 = p2.x_-p1.x_, y1 = p2.y_-p1.y_;
+    if( x1 == 0 && y1 == 0)
+      return 0;
+    float x2 = p3.x_-p1.x_, y2 = p3.y_-p1.y_;
+    if( x2 == 0 && y2 == 0)
+      return 0;
+    float A = x1*y2 - x2*y1;
+    if( A == 0)
+      return 0;
+    float p1p2_mag = (float)Math.sqrt(x1*x1 + y1*y1);
+    return A/p1p2_mag;
+  }
+  
+  
+  private final static float abs(float val){
+    return val < 0 ? -val : val;
+  }
+  
+  
+  
+}
